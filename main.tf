36940efb052af19f66181680d2257436865f755a
Nsuresh25 <nsuresh77@gmail.com>
2025-05-09 13:31:31 +0530

    terraform-aws-infra

--- main.tf
+++ main.tf
@@ -84,7 +84,7 @@ resource "aws_security_group" "ec2_sg" {
 # EC2 Instance
 resource "aws_instance" "web" {
   ami                    = "ami-0e35ddab05955cf57"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.subnet[0].id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
